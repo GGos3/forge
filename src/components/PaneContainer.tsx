a6653ae0ae9eedc94ff159ad47ac4974a560aa0e
GGos3 <dev.ggos3@gmail.com>
2026-03-15 05:04:46 +0000

    fix: preserve terminal content when closing sibling panes

--- src/components/PaneContainer.tsx
+++ src/components/PaneContainer.tsx
@@ -1,8 +1,8 @@
-import { Match, Switch } from "solid-js";
+import { Show } from "solid-js";
 import { paneStore } from "../stores/pane";
 import PaneDivider from "./PaneDivider";
 import TerminalPane from "./TerminalPane";
-import type { PaneNode } from "../types/pane";
+import type { PaneNode, SplitPane } from "../types/pane";
 import type { TabId } from "../types/tab";
 
 interface PaneContainerProps {
@@ -10,78 +10,84 @@ interface PaneContainerProps {
   node: PaneNode;
 }
 
-export default function PaneContainer(props: PaneContainerProps) {
+function SplitContainer(props: { tabId: TabId; split: SplitPane }) {
   return (
-    <Switch>
-      <Match when={props.node.type === "terminal" ? props.node : null}>
-        {(terminalNode) => (
-          <div
-            class="forge-pane-terminal-wrapper"
-            data-focused={paneStore.activePaneId === terminalNode().id}
-            onClick={() => {
-              paneStore.focusPane(terminalNode().id);
-            }}
-          >
-            <TerminalPane
-              tabId={props.tabId}
-              paneId={terminalNode().id}
-              focused={paneStore.activePaneId === terminalNode().id}
-            />
-          </div>
-        )}
-      </Match>
-      <Match when={props.node.type === "split" ? props.node : null}>
-        {(splitNode) => (
-          <div
-            class={`forge-pane-split-${splitNode().direction}`}
-            style={{
-              flex: 1,
-              display: "flex",
-              "flex-direction": splitNode().direction === "vertical" ? "row" : "column",
-              overflow: "hidden",
-              width: "100%",
-              height: "100%",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                "flex-basis": `calc(${splitNode().ratio * 100}% - 2px)`,
-                "flex-grow": 0,
-                "flex-shrink": 0,
-                overflow: "hidden",
-                width: splitNode().direction === "vertical" ? "auto" : "100%",
-                height: splitNode().direction === "horizontal" ? "auto" : "100%",
-              }}
-            >
-              <PaneContainer tabId={props.tabId} node={splitNode().first} />
-            </div>
+    <div
+      class={`forge-pane-split-${props.split.direction}`}
+      style={{
+        flex: 1,
+        display: "flex",
+        "flex-direction": props.split.direction === "vertical" ? "row" : "column",
+        overflow: "hidden",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          "flex-basis": `calc(${props.split.ratio * 100}% - 2px)`,
+          "flex-grow": 0,
+          "flex-shrink": 0,
+          overflow: "hidden",
+          width: props.split.direction === "vertical" ? "auto" : "100%",
+          height: props.split.direction === "horizontal" ? "auto" : "100%",
+        }}
+      >
+        <PaneContainer tabId={props.tabId} node={props.split.first} />
+      </div>
 
-            <PaneDivider
-              direction={splitNode().direction}
-              ratio={splitNode().ratio}
-              onRatioChange={(newRatio) => {
-                paneStore.resizeSplit(splitNode().id, newRatio);
-              }}
-            />
+      <PaneDivider
+        direction={props.split.direction}
+        ratio={props.split.ratio}
+        onRatioChange={(newRatio) => {
+          paneStore.resizeSplit(props.split.id, newRatio);
+        }}
+      />
 
-            <div
-              style={{
-                display: "flex",
-                flex: 1, // take the rest
-                "flex-basis": 0,
-                "flex-grow": 1,
-                "flex-shrink": 1,
-                overflow: "hidden",
-                width: splitNode().direction === "vertical" ? "auto" : "100%",
-                height: splitNode().direction === "horizontal" ? "auto" : "100%",
-              }}
-            >
-              <PaneContainer tabId={props.tabId} node={splitNode().second} />
-            </div>
-          </div>
-        )}
-      </Match>
-    </Switch>
+      <div
+        style={{
+          display: "flex",
+          flex: 1,
+          "flex-basis": 0,
+          "flex-grow": 1,
+          "flex-shrink": 1,
+          overflow: "hidden",
+          width: props.split.direction === "vertical" ? "auto" : "100%",
+          height: props.split.direction === "horizontal" ? "auto" : "100%",
+        }}
+      >
+        <PaneContainer tabId={props.tabId} node={props.split.second} />
+      </div>
+    </div>
+  );
+}
+
+export default function PaneContainer(props: PaneContainerProps) {
+  const isTerminal = () => props.node.type === "terminal";
+
+  return (
+    <>
+      <Show when={isTerminal()}>
+        <div
+          class="forge-pane-terminal-wrapper"
+          data-focused={paneStore.activePaneId === props.node.id}
+          onClick={() => {
+            if (props.node.type === "terminal") {
+              paneStore.focusPane(props.node.id);
+            }
+          }}
+        >
+          <TerminalPane
+            tabId={props.tabId}
+            paneId={props.node.id}
+            focused={props.node.type === "terminal" && paneStore.activePaneId === props.node.id}
+          />
+        </div>
+      </Show>
+      <Show when={!isTerminal() && props.node.type === "split" ? props.node as SplitPane : undefined}>
+        {(splitNode) => <SplitContainer tabId={props.tabId} split={splitNode()} />}
+      </Show>
+    </>
   );
 }
